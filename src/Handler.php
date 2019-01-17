ea8f427e882d9348fa4f0b388396893619961522
南山吴彦祖 <anoxia@engineer.com>
2019-01-17 13:40:46 +0800

    完成路由组件基础功能开发

---
+++ src/Handler.php
@@ -0,0 +1,77 @@
+<?php
+namespace Bee\Router;
+
+use Bee\Http\Context;
+
+/**
+ * Handler
+ *
+ * @package Star\Router
+ */
+class Handler implements HandlerInterface
+{
+    /**
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * @var array
+     */
+    protected $handler;
+
+    /**
+     * @var string
+     */
+    protected $routePattern;
+
+    /**
+     * Handler
+     *
+     * @param string $method
+     * @param string $routePattern
+     * @param array $handler
+     */
+    public function __construct(string $method, string $routePattern, array $handler)
+    {
+        $this->method       = $method;
+        $this->routePattern = $routePattern;
+        $this->handler      = $handler;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHandler(): array
+    {
+        return $this->handler;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRoutePattern(): string
+    {
+        return $this->routePattern;
+    }
+
+    /**
+     * 执行路由项业务
+     *
+     * @param Context $context
+     * @param mixed $parameters
+     * @return mixed
+     */
+    public function callMethod( $context, $parameters = null)
+    {
+        return call_user_func($this->handler, $context, $parameters);
+    }
+}
